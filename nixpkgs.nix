e8524950001eecf8c5abffb25d404fed7e360d02
Alex O'Brien <3541@3541.website>
2026-01-18 12:27:27 +1100

    Pin Nixpkgs
    This does not actually work, currently. cv4tw needs font-awesome 4,
    which is not being resolved correctly.

---
+++ nixpkgs.nix
@@ -0,0 +1,7 @@
+let
+  spec = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+in
+import (fetchTarball {
+  url = "https://github.com/${spec.repo}/tarball/${spec.commit}";
+  sha256 = spec.sha256;
+})
--- shell.nix
+++ shell.nix
@@ -1,11 +1,25 @@
-{ pkgs ? import <nixpkgs> { } }:
+# Not currently working, need FA-4.
+{
+  pkgs ? import ./nixpkgs.nix { },
+}:
 pkgs.mkShell {
-  packages = with pkgs;
-    [
-      (texlive.combine {
-        inherit (texlive)
-          scheme-medium cv4tw newenviron catoptions xstring lastpage libertine
-          pbox needspace fontawesome forloop realboxes collectbox;
-      })
-    ];
+  packages = with pkgs; [
+    (texlive.combine {
+      inherit (texlive)
+        scheme-medium
+        cv4tw
+        newenviron
+        catoptions
+        xstring
+        lastpage
+        libertine
+        pbox
+        needspace
+        fontawesome
+        forloop
+        realboxes
+        collectbox
+        ;
+    })
+  ];
 }
